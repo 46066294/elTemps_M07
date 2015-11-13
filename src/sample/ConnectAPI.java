006c8155422604d5a4b508cd6af0969506a6d011
46066294p <46066294p@iespoblenou.org>
2015-11-13 17:35:30 +0100

    parsejat JSON complert i operatiu

--- src/sample/ConnectAPI.java
+++ src/sample/ConnectAPI.java
@@ -8,6 +8,9 @@ import org.json.simple.parser.JSONParser;
 import javax.xml.transform.Transformer;
 import java.io.*;
 import java.net.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 //http://api.openweathermap.org/data/2.5/forecast/daily?q=Barcelona&mode=json&units=metric&cnt=7&appid=9d88ea129b65c04b75a6b62783fc73bb
 
@@ -37,10 +40,10 @@ public class ConnectAPI {
         String JsonTemps = "";
         //String JsonActors = "";
         String api_key = "9d88ea129b65c04b75a6b62783fc73bb";
-        String city = "Cuenca";//Modificar
+        String city = "Barcelona";//Modificar
         String units = "metric";//Modificar
         String dies = "7";
-        String url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=json&" + units + "=metric&cnt=" + dies + "&appid=9d88ea129b65c04b75a6b62783fc73bb";
+        String url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=json&units=" + units + "&cnt=" + dies + "&appid=9d88ea129b65c04b75a6b62783fc73bb";
 
         System.out.println("TEMPS:");//http://api.openweathermap.org/data/2.5/forecast/daily?q=Cuenca&mode=json&units=metric&cnt=7&appid=9d88ea129b65c04b75a6b62783fc73bb
 
@@ -90,52 +93,44 @@ public class ConnectAPI {
      * @param cadena
      */
     public static void SJS (String cadena){//para tiempo
+        Calendar fecha = new GregorianCalendar();
+        String dia, mes, annio;
+        dia = Integer.toString(fecha.get(Calendar.DATE));
+        mes = Integer.toString(fecha.get(Calendar.MONTH));
+        annio = Integer.toString(fecha.get(Calendar.YEAR));
+
         JSONObject obj02 = (JSONObject) JSONValue.parse(cadena);
 
-
         JSONObject arra02 = obj02;
-        JSONObject arra03 = obj02;
 
         String out = arra02.toJSONString();
 
         System.out.println(out);
         System.out.println();
-        //System.out.println(arra02.get("city"));
 
-        //ciudad json
-        JSONObject ciudad = (JSONObject) arra02.get("city");
-        System.out.println(ciudad.get("name"));
-        String cod = (String) arra02.get("cod");
-        System.out.println(cod);
-
-        //cod json
-        //JSONObject cod = (JSONObject) arra03.get("cod");
-        System.out.println(arra02.get("cod"));
-
-        //dia json
         JSONArray test = (JSONArray) arra02.get("list");
-        System.out.println(test.size());
+        //System.out.println(test.size());
+
+        //json parser
+        JSONObject ciudad = (JSONObject) arra02.get("city");
+        System.out.println("Ciudad: " + ciudad.get("name"));
+        String cod = (String) arra02.get("cod");
+        System.out.println("Codigo ciudad: " + cod);
+        System.out.println("Pais: " + ciudad.get("country"));
+        System.out.println("Prevision meteorologica de " + test.size() + " dias\n");
+        System.out.println("\t-----TIEMPO-----");
 
         for (int i = 0; i < test.size(); i++) {
-            //JSONArray arrayMin = (JSONArray) test.get();
-            //Object obj = test.get(i);
-            //JSONObject jobj = (JSONObject)obj;
+            //System.out.println(dia + " ");
             JSONObject weatherJsonObject = (JSONObject) test.get(i);
-            Double rain = (Double) weatherJsonObject.get("rain");
-            System.out.println(rain);
 
-            /*for(int j = 0; j < arrayMin.size(); j++){
-                //JSONObject min = (JSONObject) arrayMin.get(j);
-                //System.out.println(min.get("min"));
-                System.out.println(arrayMin.size());
-            }*/
+            JSONObject temp = (JSONObject) weatherJsonObject.get("temp");
+            String min = temp.get("min").toString();
+            String max = temp.get("max").toString();
+            System.out.println("\nTemperatura minima: " + min);
+            System.out.println("Temperatura maxima: " + max);
 
-            //System.out.println(test.get(i));
         }
-        //System.out.println(dias.get("temp"));
-
-        //System.out.println("\nTitol: " + arra02.   arra02.get("name"));
-        //System.out.println("Data de publicació: " + arra02.get("release_date"));
 
     }
 
@@ -159,195 +154,195 @@ public class ConnectAPI {
 }
 /*
 
+
+#4
+November 13th 2015, 4:37:45 pm
+Valid JSON (RFC 4627)
+Formatted JSON Data
+
+
 {
    "city":{
-      "id":3658666,
-      "name":"Cuenca",
+      "id":3128760,
+      "name":"Barcelona",
       "coord":{
-         "lon":-78.98333,
-         "lat":-2.88333
+         "lon":2.15899,
+         "lat":41.38879
       },
-      "country":"EC",
+      "country":"ES",
       "population":0
    },
    "cod":"200",
-   "message":0.0079,
+   "message":0.0082,
    "cnt":7,
    "list":[
-      0{
-         "dt":1447347600,
+      {
+         "dt":1447412400,
          "temp":{
-            "day":25.06,
-            "min":15.4,
-            "max":26.39,
-            "night":15.4,
-            "eve":19.64,
-            "morn":20.12
+            "day":16.59,
+            "min":14.71,
+            "max":16.85,
+            "night":14.71,
+            "eve":15.46,
+            "morn":16.59
          },
-         "pressure":802.02,
-         "humidity":74,
+         "pressure":1032.43,
+         "humidity":100,
          "weather":[
             {
-               "id":501,
-               "main":"Rain",
-               "description":"moderate rain",
-               "icon":"10d"
+               "id":802,
+               "main":"Clouds",
+               "description":"scattered clouds",
+               "icon":"03d"
             }
          ],
-         "speed":1.51,
-         "deg":95,
-         "clouds":64,
-         "rain":3.13
-      },
-      1 {
-         "dt":1447434000,
-         "temp":{
-            "day":22.24,
-            "min":13.07,
-            "max":23.67,
-            "night":13.07,
-            "eve":17.79,
-            "morn":16.61
-         },
-         "pressure":801.77,
-         "humidity":76,
-         "weather":[
-            {
-               "id":500,
-               "main":"Rain",
-               "description":"light rain",
-               "icon":"10d"
-            }
-         ],
-         "speed":1.55,
-         "deg":84,
-         "clouds":80,
-         "rain":1.32
+         "speed":1.11,
+         "deg":76,
+         "clouds":36
       },
       {
-         "dt":1447520400,
+         "dt":1447498800,
          "temp":{
-            "day":20.39,
-            "min":9.47,
-            "max":20.39,
-            "night":9.47,
-            "eve":15.13,
-            "morn":14.07
+            "day":18.04,
+            "min":12.9,
+            "max":18.18,
+            "night":14.46,
+            "eve":16.5,
+            "morn":12.9
          },
-         "pressure":802.58,
-         "humidity":73,
+         "pressure":1033.33,
+         "humidity":92,
          "weather":[
             {
-               "id":500,
-               "main":"Rain",
-               "description":"light rain",
-               "icon":"10d"
+               "id":800,
+               "main":"Clear",
+               "description":"sky is clear",
+               "icon":"01d"
             }
          ],
-         "speed":1.51,
-         "deg":94,
-         "clouds":36,
-         "rain":1.48
+         "speed":1.94,
+         "deg":264,
+         "clouds":0
       },
       {
-         "dt":1447606800,
+         "dt":1447585200,
          "temp":{
-            "day":20.72,
-            "min":11.5,
-            "max":20.73,
-            "night":11.5,
-            "eve":15.58,
-            "morn":13.75
+            "day":17.2,
+            "min":12.8,
+            "max":18.16,
+            "night":15.08,
+            "eve":16.19,
+            "morn":12.8
          },
-         "pressure":802.17,
-         "humidity":68,
+         "pressure":1027.02,
+         "humidity":96,
          "weather":[
             {
-               "id":500,
-               "main":"Rain",
-               "description":"light rain",
-               "icon":"10d"
+               "id":800,
+               "main":"Clear",
+               "description":"sky is clear",
+               "icon":"01d"
             }
          ],
-         "speed":1.92,
-         "deg":95,
-         "clouds":48,
-         "rain":2.78
+         "speed":2.66,
+         "deg":249,
+         "clouds":0
       },
       {
-         "dt":1447693200,
+         "dt":1447671600,
          "temp":{
-            "day":20.3,
-            "min":12.36,
-            "max":20.3,
-            "night":12.36,
-            "eve":14.89,
-            "morn":13.35
+            "day":19.33,
+            "min":14.03,
+            "max":19.41,
+            "night":15.17,
+            "eve":16.71,
+            "morn":14.03
          },
-         "pressure":802.08,
+         "pressure":1024.86,
+         "humidity":83,
+         "weather":[
+            {
+               "id":800,
+               "main":"Clear",
+               "description":"sky is clear",
+               "icon":"01d"
+            }
+         ],
+         "speed":0.81,
+         "deg":318,
+         "clouds":0
+      },
+      {
+         "dt":1447758000,
+         "temp":{
+            "day":18.67,
+            "min":12.91,
+            "max":18.88,
+            "night":14.72,
+            "eve":16.06,
+            "morn":12.91
+         },
+         "pressure":1025.85,
+         "humidity":86,
+         "weather":[
+            {
+               "id":800,
+               "main":"Clear",
+               "description":"sky is clear",
+               "icon":"01d"
+            }
+         ],
+         "speed":1.26,
+         "deg":315,
+         "clouds":0
+      },
+      {
+         "dt":1447844400,
+         "temp":{
+            "day":17.87,
+            "min":13.95,
+            "max":17.87,
+            "night":14.83,
+            "eve":16.12,
+            "morn":13.95
+         },
+         "pressure":1023.94,
          "humidity":0,
          "weather":[
             {
-               "id":501,
-               "main":"Rain",
-               "description":"moderate rain",
-               "icon":"10d"
+               "id":800,
+               "main":"Clear",
+               "description":"sky is clear",
+               "icon":"01d"
             }
          ],
-         "speed":0.85,
-         "deg":148,
-         "clouds":34,
-         "rain":5.66
+         "speed":3.07,
+         "deg":254,
+         "clouds":0
       },
       {
-         "dt":1447779600,
+         "dt":1447930800,
          "temp":{
-            "day":19.15,
-            "min":12.57,
-            "max":19.15,
-            "night":12.57,
-            "eve":14.21,
-            "morn":13.2
+            "day":16.93,
+            "min":13.31,
+            "max":16.93,
+            "night":14.25,
+            "eve":15.85,
+            "morn":13.31
          },
-         "pressure":801.75,
+         "pressure":1024.37,
          "humidity":0,
          "weather":[
             {
-               "id":501,
-               "main":"Rain",
-               "description":"moderate rain",
-               "icon":"10d"
+               "id":800,
+               "main":"Clear",
+               "description":"sky is clear",
+               "icon":"01d"
             }
          ],
-         "speed":0.37,
-         "deg":180,
-         "clouds":32,
-         "rain":9.77
-      },
-      {
-         "dt":1447866000,
-         "temp":{
-            "day":18.42,
-            "min":12.66,
-            "max":18.42,
-            "night":13.33,
-            "eve":14.53,
-            "morn":12.66
-         },
-         "pressure":801.63,
-         "humidity":0,
-         "weather":[
-            {
-               "id":501,
-               "main":"Rain",
-               "description":"moderate rain",
-               "icon":"10d"
-            }
-         ],
-         "speed":0.57,
-         "deg":173,
-         "clouds":24,
-         "rain":11.53
+         "speed":2.69,
+         "deg":277,
+         "clouds":0
       }
    ]
 }
